74a556ed12905b395b2a70aa6d18060298b3dc2d
winkula <mathiaswinkler89@gmail.com>
2020-07-04 00:18:37 +0200

    bugfix in enumeration function

--- pkg/model/enumerate.go
+++ pkg/model/enumerate.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Options defines possible square values when enumerating valid world states.
 var options = []Square{SquareDragon, SquareFire, SquareEmpty}
 
@@ -18,28 +23,37 @@ func hasSquaresOfType(w *World, square Square) bool {
 }
 
 func enumerateInternal(w *World, result []*World, index int) []*World {
+	if !ValidateWorld(w) {
+		// skip further investigation because the state is invalid
+		fmt.Printf("%vInvalid!\n", strings.Repeat(" ", index))
+		fmt.Println(w)
+		return result
+	}
+
+	if !hasSquaresOfType(w, SquareUndefined) {
+		// only append, if its a leaf node (final state, no undefined squares)
+		fmt.Println("Sucessor found!")
+		result = append(result, w)
+		return result
+	}
+
 	if index >= len(w.Squares) {
-		return result // stop recursion
+		panic("Force recursio stop (this should never happen)!!!")
 	}
 
 	x, y := w.GetCoords(index)
 	square := w.GetSquare(x, y)
-	if square != SquareUndefined {
-		return result // square already defined
-	}
-
-	//fmt.Printf("Enumerate i=%v, x=%v, y=%v", i, x, y)
-	for _, option := range options {
-		// fmt.Printf("Enumerate (option %v) for world: %s", option, w)
-		successor := w.Clone()
-		successor.SetSquare(x, y, option)
-		if ValidateWorld(successor) {
-			if !hasSquaresOfType(successor, SquareUndefined) {
-				// only append, if its a leaf node (final state, no undefined squares)
-				result = append(result, successor)
-			}
+	if square == SquareUndefined {
+		for _, option := range options {
+			fmt.Printf("%vOption %c for x=%v,y=%v\n", strings.Repeat(" ", index), squareSymbols[option], x, y)
+			successor := w.Clone()
+			successor.SetSquare(x, y, option)
 			result = enumerateInternal(successor, result, index+1)
 		}
+	} else {
+		fmt.Printf("%vSquare already defined for i=%v x=%v,y=%v\n", strings.Repeat(" ", index), index, x, y)
+		successor := w.Clone()
+		result = enumerateInternal(successor, result, index+1)
 	}
 	return result
 }
